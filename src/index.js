12e23c21e43a9f2555b3c6b611addd8c0ab7131e
Rafael Albuquerque <rafael.silva.albuquerque@hotmail.com>
2024-09-10 15:20:06 -0300

    view game state

--- src/index.js
+++ src/index.js
@@ -20,23 +20,44 @@ let caught = false;
 
 while (!caught) {
   console.log(`Rodada ${round}:`);
-  
+
   // Mostrar a posição do Pac-Man e do Fantasma
   console.log(`Posição do Pac-Man: ${player.position}`);
   console.log(`Posição do Fantasma: ${ghost.position}`);
-  
+
+  console.log('\nEstado do jogo:');
+  let gameState = maze.map((arr) => { return arr.slice(); });
+  gameState[player.position[0]][player.position[1]] = 2
+  gameState[ghost.position[0]][ghost.position[1]] = 3
+  gameState.forEach((i) => { console.log(i); });
+  console.log();
+
   // O fantasma se move em direção ao Pac-Man
   ghost.chase(player, maze);
-  
+
   // Verificar se o fantasma capturou o Pac-Man
   if (ghost.position[0] === player.position[0] && ghost.position[1] === player.position[1]) {
     console.log('O Fantasma capturou o Pac-Man!');
     caught = true;
+
+    console.log('\nEstado do jogo:');
+    let gameState = maze.map((arr) => { return arr.slice(); });
+    gameState[player.position[0]][player.position[1]] = 2
+    gameState[ghost.position[0]][ghost.position[1]] = 3
+    gameState.forEach((i) => { console.log(i); });
+    console.log();
   }
-  
+
   // Adicionar um limite de rodadas para parar o loop (para evitar infinitos)
   if (round >= 10) {
     console.log('Limite de rodadas atingido.');
+
+    console.log('\nEstado do jogo:');
+    let gameState = maze.map((arr) => { return arr.slice(); });
+    gameState[player.position[0]][player.position[1]] = 2
+    gameState[ghost.position[0]][ghost.position[1]] = 3
+    gameState.forEach((i) => { console.log(i); });
+    console.log();
     break;
   }
 
