58802feb41ed5a88dacfa8ac77e4d3695e7f0e01
Rafael Albuquerque <rafael.silva.albuquerque@hotmail.com>
2024-09-10 18:43:59 -0300

    options and table

--- src/game.js
+++ src/game.js
@@ -13,21 +13,58 @@ export default class Pacman {
             [-1, 0], //cima
             [1, 0],  // baixo
             [0, -1], // esquerda
-            [0, 1]   // direita 
+            [0, 1]   // direita
         ];
     }
 
+<<<<<<< HEAD
+=======
+    // bfs(start, end) {
+    //     let queue = [[start]];
+    //     let visited = Array.from({
+    //         length: this.maze.length
+    //     }, () => Array(this.maze[0].length).fill(false));
+
+    //     visited[start[0]] [start[1]] = true;
+
+    //     while(queue.length > 0) {
+    //         let path = queue.shift();
+    //         let [x, y] = path[path.length - 1];
+
+    //         if (x === end[0] && y === end[1]) {
+    //             return path;
+    //         }
+
+    //         for (let [dx, dy] of this.directions) {
+    //             let newX = x + dx;
+    //             let newY = y + dy;
+
+    //             if (
+    //                 newX >= 0 && newX < this.maze.rows &&
+    //                 newY >= 0 && newY < this.maze.cols &&
+    //                 this.maze.isWalkable(newX, newY) &&
+    //                 !visited[newX][newY]
+    //              ) {
+    //                 visited[newX][newY] = true;
+    //                 queue.push([...path, [newX, newY]]);
+    //              }
+    //         }
+    //     }
+    //     return null;
+    // }
+
+>>>>>>> 0957621 (options and table)
     moveGhosts() {
         this.ghosts.forEach(ghost => {
             let path = ghost.chase(this.players[0], this.maze);
             if (path && path.length > 1) {
                 let [nextX, nextY] = path[1];
                 ghost.move([nextX, nextY]);
-    
-                if (nextX === this.players[0].position[0] && 
+
+                if (nextX === this.players[0].position[0] &&
                     nextY === this.players[0].position[1]) {
                     console.log(`${ghost.name} capturou o ${this.players[0].name}!`);
-                    
+
                     process.exit();  // Para a execução do jogo
                 } else {
                     console.log(`${ghost.name} moveu para a posição [${nextX}, ${nextY}]`);
@@ -37,10 +74,30 @@ export default class Pacman {
             }
         });
     }
+<<<<<<< HEAD
     
+=======
+
+
+>>>>>>> 0957621 (options and table)
     simulateGame(rounds) {
         for (let i = 0; i < rounds; i++) {
             this.moveGhosts();
         }
+    }
+
+    printGameState(maze, ghost, player) {
+        console.log('\nEstado do jogo:');
+        let gameState = maze.map((arr, i) => {
+            // let row = new Object();
+            // row[i] = arr.slice();
+            // return row;
+            return arr.slice();
+        });
+        gameState[player.position[0]][player.position[1]] = 2;
+        gameState[ghost.position[0]][ghost.position[1]] = 3;
+        console.table(gameState);
+        // gameState.forEach((i) => { console.log(i[i]); });
+        console.log();
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,54 @@
-import Maze from "./maze.js";
 import Player from "./player.js";
 import Ghost from "./ghost.js";
 import Pacman from "./game.js";
 
-const maze = [
+const x = Number(process.argv[2]);
+
+const mazes = [
+  [
     [0, 1, 0, 0],
     [0, 1, 0, 1],
     [0, 0, 0, 1],
     [1, 1, 0, 0],
-  ];
-const ghostPosition = [0, 0];  // Posição inicial do fantasma
-const pacmanPosition = [3, 3];  // Posição inicial do Pac-Man
+  ],
+  [
+    [0, 1, 0, 0, 0],
+    [0, 1, 1, 0, 1],
+    [0, 0, 1, 0, 0],
+    [1, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+  ],
+  [
+    [0, 1, 0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 1, 0, 1],
+    [0, 1, 1, 0, 1, 0, 1],
+    [0, 0, 1, 0, 1, 0, 1],
+    [0, 1, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 1, 1],
+  ],
+];
+const maze = mazes[x]?? mazes[0];
+
+const positions = [
+  {
+    "ghost": [0, 0],
+    "pacman": [3, 3]
+  },
+  {
+    "ghost": [0, 0],
+    "pacman": [0, 4]
+  },
+  {
+    "ghost": [6, 0],
+    "pacman": [3, 3]
+  }
+];
+
+const ghostPosition = positions[x]["ghost"];  // Posição inicial do fantasma
+const pacmanPosition = positions[x]["pacman"];  // Posição inicial do Pac-Man
+
+const pacman = new Pacman();
 
 const player = new Player('Pacman', pacmanPosition);
 const ghost = new Ghost('Fantasma vermelho', ghostPosition);
@@ -25,12 +63,13 @@ while (!caught) {
   console.log(`Posição do Pac-Man: ${player.position}`);
   console.log(`Posição do Fantasma: ${ghost.position}`);
 
-  console.log('\nEstado do jogo:');
-  let gameState = maze.map((arr) => { return arr.slice(); });
-  gameState[player.position[0]][player.position[1]] = 2
-  gameState[ghost.position[0]][ghost.position[1]] = 3
-  gameState.forEach((i) => { console.log(i); });
-  console.log();
+  // console.log('\nEstado do jogo:');
+  // let gameState = maze.map((arr) => { return arr.slice(); });
+  // gameState[player.position[0]][player.position[1]] = 2
+  // gameState[ghost.position[0]][ghost.position[1]] = 3
+  // gameState.forEach((i) => { console.log(i); });
+  // console.log();
+  pacman.printGameState(maze, player, ghost);
 
   // O fantasma se move em direção ao Pac-Man
   ghost.chase(player, maze);
@@ -40,26 +79,16 @@ while (!caught) {
     console.log('O Fantasma capturou o Pac-Man!');
     caught = true;
 
-    console.log('\nEstado do jogo:');
-    let gameState = maze.map((arr) => { return arr.slice(); });
-    gameState[player.position[0]][player.position[1]] = 2
-    gameState[ghost.position[0]][ghost.position[1]] = 3
-    gameState.forEach((i) => { console.log(i); });
-    console.log();
+    pacman.printGameState(maze, player, ghost);
   }
 
   // Adicionar um limite de rodadas para parar o loop (para evitar infinitos)
-  if (round >= 10) {
+  if (round >= 100) {
     console.log('Limite de rodadas atingido.');
-
-    console.log('\nEstado do jogo:');
-    let gameState = maze.map((arr) => { return arr.slice(); });
-    gameState[player.position[0]][player.position[1]] = 2
-    gameState[ghost.position[0]][ghost.position[1]] = 3
-    gameState.forEach((i) => { console.log(i); });
-    console.log();
+    pacman.printGameState(maze, player, ghost);
     break;
   }
 
+  console.log();
   round++;
 }
